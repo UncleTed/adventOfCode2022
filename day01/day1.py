b2b7723581b01ff8c192fe8350781e4f9f0a602d
Ted <phred@bluebeard>
2022-12-02 17:18:42 -0500

    day1 done

--- day01/day1.py
+++ day01/day1.py
@@ -1,9 +1,19 @@
 with open("./long.txt", "r") as f:
     total = 0
+    carrying = []
     for line in f:
         l = line.rstrip();
         if (l == ''):
-            print('total: ', total)
+            # print('total: ', total)
+            carrying.append(total)
             total = 0
         else: 
             total += int(l);
+    
+    s = sorted(carrying, reverse=True)
+    print (s[0])
+    print (s[0] + s[1] + s[2])
+
+    # print(sorted(carrying)[-1], sorted(carrying)[-2], sorted(carrying)[-3])
+    # last three are 68787 65169 64085
+    #199041 is too high
